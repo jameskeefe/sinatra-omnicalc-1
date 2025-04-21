873e654a55ff443778076c4174990dd21efa1a76
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-21 00:02:25 +0000

    First half of assignment

--- app.rb
+++ app.rb
@@ -2,8 +2,43 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/square/") do
+  @num = params[:number].to_f
+  @calc = @num**2
+  erb(:square_results)
+end
+
+get("/square_root/") do
+  @num = params[:number].to_f
+  @calc = @num**(1.0/2.0)
+  erb(:square_root_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/random/") do
+
+  @min = params[:user_min].to_f
+  @max = params[:user_max].to_f
+
+  @calc = rand(@min..@max)
+
+  erb(:random_results)
+end
+
+get("/payment/new") do
+
 end
